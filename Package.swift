33b94f92fe0895aa6bb1f6d85803b897c5417c1f
Marius Serban <marius.serban@protonmail.com>
2017-01-04 17:05:49 +0000

    add S4 dependency as an interface to an HTTP client

--- Package.swift
+++ Package.swift
@@ -1,5 +1,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "monzo-swift"
+    name: "monzo-swift",
+    dependencies: [
+        .Package(url: "https://github.com/marius-serban/S4.git", "0.12.2")
+    ]
 )
