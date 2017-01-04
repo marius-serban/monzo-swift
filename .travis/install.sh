5c3b64be577073f218f222ff9feb85327ca0be45
Marius Serban <marius.serban@protonmail.com>
2017-01-04 10:36:52 +0000

    configure ci build with travis

---
+++ .travis/install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+git clone --depth 1 https://github.com/kylef/swiftenv.git ~/.swiftenv
+export SWIFTENV_ROOT="$HOME/.swiftenv"
+export PATH="$SWIFTENV_ROOT/bin:$SWIFTENV_ROOT/shims:$PATH"
+
+if [ -f ".swift-version" ] || [ -n "$SWIFT_VERSION" ]; then
+  swiftenv install
+else
+  swiftenv rehash
+fi
