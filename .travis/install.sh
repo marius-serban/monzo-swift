a2404bc0ef5021a31df1c7ed80ea0c55dd7ad27b
Marius Serban <marius.serban@protonmail.com>
2017-01-04 11:22:28 +0000

    fix travis build for osx

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,12 +2,19 @@
 
 set -e
 
+# install swiftenv
 git clone --depth 1 https://github.com/kylef/swiftenv.git ~/.swiftenv
 export SWIFTENV_ROOT="$HOME/.swiftenv"
 export PATH="$SWIFTENV_ROOT/bin:$SWIFTENV_ROOT/shims:$PATH"
 
+# install swift using swiftenv
 if [ -f ".swift-version" ] || [ -n "$SWIFT_VERSION" ]; then
   swiftenv install
 else
   swiftenv rehash
+fi
+
+# workaround travis bug https://github.com/travis-ci/travis-ci/issues/6307
+if [[ $(uname) == "Darwin" ]]; then
+    rvm get stable
 fi
