918974166bc4befcbc8bb3d0494082e7a2e67a65
Marius Serban <marius.serban@protonmail.com>
2017-01-08 00:04:10 +0000

    add method that generates an authorization URI

--- Tests/monzo-swiftTests/monzo_swiftTests.swift
+++ Tests/monzo-swiftTests/monzo_swiftTests.swift
@@ -2,7 +2,7 @@ import XCTest
 import S4
 @testable import monzo_swift
 
-class monzo_swiftTests: XCTestCase {
+class monzo_swiftTests : XCTestCase {
     
     func test_givenAHttpClient_whenInitialialised_thenHttpClientIsSet() {
         let httpClient = StubHttpClient(response: fakeResponse)
@@ -12,14 +12,27 @@ class monzo_swiftTests: XCTestCase {
         XCTAssertTrue(sut.httpClient is StubHttpClient)
     }
     
+    func test_givenClientIdAndRedirectUriAndNonce_whenGeneratingAuthenticationUri_thenAuthenticationUriIsCorrect() {
+        guard let uri = try? MonzoClient.authenticationUri(clientId: "aClientId", redirectUri: "http://monzo.com/?test=[]#fragment", nonce: "abc123") else { XCTFail(#function); return }
+        
+        XCTAssertEqual(uri.scheme, "https")
+        XCTAssertEqual(uri.host, "auth.getmondo.co.uk")
+        XCTAssertEqual(uri.query, "client_id=aClientId&redirect_uri=http://monzo.com/?test=%5B%5D%23frag&state=abc123")
+        XCTAssertNil(uri.userInfo)
+        XCTAssertNil(uri.port)
+        XCTAssertNil(uri.path)
+        XCTAssertNil(uri.fragment)
+    }
+    
     static var allTests : [(String, (monzo_swiftTests) -> () throws -> Void)] {
         return [
             ("test_givenAHttpCleint_whenInitialised_thenHttpClientIsSet", test_givenAHttpClient_whenInitialialised_thenHttpClientIsSet),
+            ("test_givenClientIdAndRedirectUriAndNonce_whenGeneratingAuthenticationUri_thenAuthenticationUriIsCorrect", test_givenClientIdAndRedirectUriAndNonce_whenGeneratingAuthenticationUri_thenAuthenticationUriIsCorrect),
         ]
     }
 }
 
-struct StubHttpClient: Responder {
+struct StubHttpClient : Responder {
     let response: Response
     
     func respond(to request: Request) throws -> Response {
