1e7f13eaeab1106c7dea58482b922fc68a9b0ec5
Marius Serban <marius.serban@protonmail.com>
2017-02-08 11:24:24 +0000

    add support for refreshing an expired access token

--- README.md
+++ README.md
@@ -6,14 +6,13 @@
 [![Travis][travis-badge]][travis-url]
 [![Codebeat][codebeat-badge]][codebeat-url]
 
-A Monzo client written in Swift that provides a simple interface to the Monzo API. This package is targeted towards server side use on Linux; if you're looking for a Monzo client for iOS then have a look at [MondoKit](https://github.com/pollarm/MondoKit).
+A Monzo client that provides a simple Swift interface to the Monzo API. This package is targeted towards server side use on Linux; if you're looking for a Monzo client for iOS then have a look at [MondoKit](https://github.com/pollarm/MondoKit).
 
 API documentation, user guides and setup information can be found at [monzo.com/docs](https://monzo.com/docs/).
 
 ## Installation
 
-**Note:** the minimum required swift version is `DEVELOPMENT-SNAPSHOT-2016-11-08-a`
-
+**Note:** the minimum required Swift version is `DEVELOPMENT-SNAPSHOT-2016-11-08-a`
 ```swift
 import PackageDescription
 
@@ -24,9 +23,54 @@ let package = Package(
 )
 ```
 
+## Getting started
+The library is compatible with [Open Swift][open-swift] so you can use it with any server side framework that supports this standards. It relies upon a [S4][s4] `Responder` to perform the HTTP calls over the network. Luckly, [Vapor][vapor]'s `Droplet` and [Zewo][zewo]'s' `Client` conform to this protocol for example.
+
+### Initialize your client
+
+```swift
+let myHttpClient = ...
+let monzo = Monzo.Client(httpClient: myHttpClient)
+```
+
+### Create authorization URI
+
+Create a URI that points to the Monzo login page for your app.
+
+```swift
+let uri = Monzo.Client.authorizationUri(clientId: "aClientId", redirectUri: "http://host.com/?param=[]#fragment", nonce: "abc123")
+// redirect user to URI
+```
+
+### Authenticate
+
+```swift
+let credentials = try monzo.authenticate(withCode: " ", clientId: " ", clientSecret: " ")
+```
+
+### Ping
+
+```swift
+do {
+  try monzo.ping()
+} catch {
+  // try again later
+}
+```
+
+### Whoami
+
+```swift
+let accessTokenInfo = try monzo.whoami(accessToken: "a_token")
+```
+
+
 ## Support
 
 You can create a Github [issue](https://github.com/marius-serban/monzo-swift/issues/new) in this repository. When stating your issue be sure to add enough details about what's causing the problem and reproduction steps.
+
+
+Also, you can get in touch with me on [Twitter][my-twitter].
 
 ## License
 
@@ -41,3 +85,8 @@ This project is released under the MIT license. See [LICENSE](LICENSE) for detai
 [codebeat-badge]: https://codebeat.co/badges/b5c058fb-f26d-4a8d-82f9-82904b542c33
 [codebeat-url]: https://codebeat.co/projects/github-com-marius-serban-monzo-swift
 [linux]: https://img.shields.io/badge/Platform-linux-orange.svg
+[open-swift]: https://github.com/open-swift
+[s4]: https://github.com/open-swift/S4
+[vapor]: http://vapor.codes/
+[zewo]: http://www.zewo.io/
+[my-twitter]: https://www.twitter.com/smarius
---
+++ Tests/MonzoTests/RefreshAccessTokenTests.swift
@@ -0,0 +1,90 @@
+import XCTest
+import S4
+import Monzo
+
+class RefreshAccessTokenTests : XCTestCase {
+    
+    func test_requestHasCorrectMethod() {
+        let method = request(forClientAction: { sut in
+            try sut.refreshAccessToken(refreshToken: "", clientId: "", clientSecret: "")
+        }).method
+        
+        XCTAssertEqual(method, Method.post)
+    }
+    
+    func test_requestHasCorrectUri() {
+        let uri = request(forClientAction: { sut in
+            try sut.refreshAccessToken(refreshToken: "", clientId: "", clientSecret: "")
+        }).uri
+        
+        XCTAssertEqual(uri.debugDescription, "https://api.monzo.com/oauth2/token")
+    }
+    
+    func test_requestHasCorrectHeaders() {
+        let headers = request(forClientAction: { sut in
+            try sut.refreshAccessToken(refreshToken: "", clientId: "", clientSecret: "")
+        }).headers.headers
+        
+        XCTAssertEqual(headers, [
+            "host": "api.monzo.com",
+            "connection": "close",
+            "content-type": "application/x-www-form-urlencoded; charset=utf-8"
+            ])
+    }
+    
+    func test_givenRefreshParameters_thenTheRequestHasCorrectParametersInBody() {
+        let body = requestBody(forClientAction: { sut in
+            try sut.refreshAccessToken(refreshToken: ":/?#[]@!$&'()*+,;=🌎-._~ ", clientId: "a string with spaces", clientSecret: "")
+        })
+        
+        XCTAssertNotNil(body?.range(of: "grant_type=refresh_token"))
+        XCTAssertNotNil(body?.range(of: "client_id=a+string+with+spaces"))
+        XCTAssertNotNil(body?.range(of: "client_secret="))
+        XCTAssertNotNil(body?.range(of: "refresh_token=%3A%2F%3F%23%5B%5D%40%21%24%26%27%28%29%2A%2B%2C%3B%3D%F0%9F%8C%8E-._~+"))
+    }
+    
+    func test_givenASucessfulResponse_thenCorrectCredentialsAreReturned() {
+        let sut = configuredClient(withResponseBody: "{\"access_token\":\"a token\",\"client_id\":\"a client id\",\"expires_in\":21599,\"refresh_token\":\"a refresh token\",\"token_type\":\"Bearer\",\"user_id\":\"a user id\"}")
+        
+        do {
+            let credentials = try sut.refreshAccessToken(refreshToken: "", clientId: "", clientSecret: "")
+            XCTAssertEqual(credentials.accessToken, "a token")
+            XCTAssertEqual(credentials.clientId, "a client id")
+            XCTAssertEqual(credentials.expiresIn, 21599)
+            XCTAssertEqual(credentials.refreshToken, "a refresh token")
+            XCTAssertEqual(credentials.tokenType, "Bearer")
+            XCTAssertEqual(credentials.userId, "a user id")
+        } catch {
+            XCTFail(String(describing: error))
+        }
+    }
+    
+    func test_givenAnInvalidResponseStatus_thenResponseErrorIsThrown() {
+        let sut = configuredClient(withResponseBody: "", responseStatus: .badRequest)
+        
+        XCTAssertThrowsError(try sut.refreshAccessToken(refreshToken: "", clientId: "", clientSecret: ""), #function) { error in
+            guard case Monzo.ClientError.responseError(.badRequest) = error else { XCTFail(#function); return }
+        }
+    }
+    
+    func test_givenAnInvalidResponseBody_thenParsingErrorIsThrown() {
+        let sut = configuredClient(withResponseBody: "{}")
+        
+        XCTAssertThrowsError(try sut.refreshAccessToken(refreshToken: "", clientId: "", clientSecret: ""), #function) { error in
+            guard case Monzo.ClientError.parsingError = error else { XCTFail(#function); return }
+        }
+    }
+    
+    static var allTests : [(String, (RefreshAccessTokenTests) -> () throws -> Void)] {
+        return [
+            ("test_requestHasCorrectMethod", test_requestHasCorrectMethod),
+            ("test_requestHasCorrectUri", test_requestHasCorrectUri),
+            ("test_requestHasCorrectHeaders", test_requestHasCorrectHeaders),
+            ("test_givenRefreshParameters_thenTheRequestHasCorrectParametersInBody", test_givenRefreshParameters_thenTheRequestHasCorrectParametersInBody),
+            ("test_givenASucessfulResponse_thenCorrectCredentialsAreReturned", test_givenASucessfulResponse_thenCorrectCredentialsAreReturned),
+            ("test_givenAnInvalidResponseStatus_thenResponseErrorIsThrown", test_givenAnInvalidResponseStatus_thenResponseErrorIsThrown),
+            ("test_givenAnInvalidResponseBody_thenParsingErrorIsThrown", test_givenAnInvalidResponseBody_thenParsingErrorIsThrown),
+        ]
+    }
+    
+}
